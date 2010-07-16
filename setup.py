e5eb41ca1407f4fd9019a7fc4f1b2d39eb06f823
Francesco Facconi <francesco@facconi.eu>
2010-07-16 14:10:55 +0000

    Released version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-provinceitaliane',
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'Province e Regioni Italiane for Django',
     author = 'Francesco Facconi',
     url = 'http://code.google.com/p/django-provinceitaliane/',
