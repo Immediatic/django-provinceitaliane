5cb4d96bd5150a580a9328c07c9e7f20037664b5
Francesco Facconi <francesco@facconi.eu>
2010-07-16 13:16:07 +0000

    First commit of project, release 0.1.0

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-provinceitaliane',
+    version = '0.1.0',
+    description = 'Province e Regioni Italiane for Django',
+    author = 'Francesco Facconi',
+    url = 'http://code.google.com/p/django-provinceitaliane/',
+    packages = find_packages(),
+    zip_safe=False,
+    package_data = {
+        '': ['fixtures/initial_data.json', 'locale/*'],
+    },
+)
